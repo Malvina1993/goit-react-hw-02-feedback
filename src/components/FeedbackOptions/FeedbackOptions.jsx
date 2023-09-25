71db40d9c668b7cdfc1206fe984efe94a0e263f7
Malvina1993 <mykhamalvina@gmail.com>
2023-09-25 23:31:07 +0300

    my first commit for all

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,12 @@
+import css from "./FeedbackOptions.module.css";
+
+export const FeedbackOptions = ({ option, onLeaveFeedback}) => {
+    return (
+       
+        <div className = {css.feedback}>
+            <button name="good" className = {css.btnOpt} onClick={onLeaveFeedback}>Good</button>
+            <button name = "neutral" className = {css.btnOpt} onClick={onLeaveFeedback}>Neutral</button>
+            <button name="bad" className = {css.btnOpt} onClick={onLeaveFeedback}>Bad</button>
+        </div>
+    )
+}
---
+++ src/components/Statistics/Statistic.jsx
@@ -0,0 +1,15 @@
+import css from './Statistics.module.css';
+
+export const Statistic = ({good, neutral, bad, total,positivePercentage}) => {
+    return (
+        <div>
+            <ul className={css.statistic}>
+                <li>Good: {good}</li>
+                <li>Neutral: {neutral}</li>
+                <li>Bad: {bad}</li>
+                <li>Total: {total}</li>
+                <li>Positive feedback: {positivePercentage}%</li>
+            </ul>
+        </div>
+    )
+}
